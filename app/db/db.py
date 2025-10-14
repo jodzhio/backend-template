951d24b5fe8162d9802b666892ef7ccbba88c4d2
jodzhio <mikuakidera@gmail.com>
2025-10-14 16:52:29 +0300

    mainly init commit

---
+++ app/db/db.py
@@ -0,0 +1,16 @@
+from collections.abc import AsyncGenerator
+from typing import Any
+
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, AsyncEngine
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = ""
+
+engine: AsyncEngine = create_async_engine(DATABASE_URL, echo=True)
+
+Session = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+
+
+async def db_session() -> AsyncGenerator[AsyncSession, Any]:
+    async with Session() as session:
+        yield session
---
+++ app/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+if __name__ == "__main__":
+    uvicorn.run(
+        app=app, 
+        host="localhost", 
+        port=8000, 
+        reload=True,
+        factory=True,
+    )
