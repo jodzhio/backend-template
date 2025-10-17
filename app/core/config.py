52110b90fb8ba11fe52f7227d387730f16c2b8ae
jodzhio <mikuakidera@gmail.com>
2025-10-18 01:39:05 +0300

    add redis url

--- app/core/config.py
+++ app/core/config.py
@@ -11,10 +11,21 @@ class Settings(BaseSettings):
     redis_user: str
     redis_user_password: str
     redis_password: str
+    redis_host: str
+    redis_port: int
 
     model_config = SettingsConfigDict(env_file=".env.example")
     @property
     def DB_URL(self) -> str:
         return f"postgresql+asyncpg://{self.db_user}:{self.db_pass}@{self.db_host}:{self.db_port}/{self.db_base}"
+
+    @property
+    def REDIS_URL(self) -> str:
+        if self.redis_user and self.redis_user_password:
+            return f"redis://{self.redis_user}:{self.redis_user_password}@{self.redis_host}:{self.redis_port}/0"
+        elif self.redis_password:
+            return f"redis://:{self.redis_password}@{self.redis_host}:{self.redis_port}/0"
+        else:
+            return f"redis://{self.redis_host}:{self.redis_port}/0"
 
 settings = Settings()
